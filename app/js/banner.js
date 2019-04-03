ef58a16f47f8010aff6addd601b2f5acd9021b74
Andy1027703354 <1027703354@qq.com>
2019-04-03 10:54:06 +0800

    改了

--- app/js/banner.js
+++ app/js/banner.js
@@ -72,7 +72,7 @@ class Banner {
             timer = setInterval(() => {
                 index++;
                 img();
-            }, 2000)
+            }, 3000)
         }
         init();
     }
@@ -224,9 +224,404 @@ class CountDown {
         }, 1000)
     }
 }
+//修改中
+class RanNav{
+    constructor() {
+        this.ranNav = document.querySelector('.ran-nav');
+        this.ranUl = this.ranNav.querySelector('.ran-ul');
+        this.ranUlAll = document.querySelectorAll('.ran-nav .ran-ul');
+        this.all = this.ranNav.querySelectorAll('.navBox a');
+        this.ranUlbox = this.ranNav.querySelector('.ran-ul-box');
+        this.ranNavBox = this.ranNav.querySelector('.navBox');
+        this.ranNavAll = this.ranNavBox.children;
+        this.ranUlboxAll = this.ranUlbox.children;
+        this.flist = this.ranUlboxAll[0];
+        this.last = this.ranUlboxAll[this.ranUlboxAll.length - 1];
+        this.ranUlbox.appendChild(this.flist.cloneNode(true));
+        this.ranUlbox.insertBefore(this.last.cloneNode(true), this.flist);
+        this.rubIndex = this.ranUl.children[1];
+        this.rubIndexAll = this.rubIndex.children;
+        this.index = 0;
+        this.count = 0;
+        this.timer = null;
+        this.ranUlbox.style.left = -350 + 'px';
+        for (var i = 0; i < this.rubIndexAll.length; i++) {
+            this.rubIndexAll[i].index = i;
+        }
+        for(var j=0;j<this.ranNavAll.length;j++){
+            this.ranNavAll[j].count = j;
+            this.ranNavAll[j].children[0].count = j;
+        }
+       // this.autoplay();
+    }
+    event() {
+        this.rubIndex.onclick = (e) => {
+            e = e || event;
+            var target = e.target || e.srcElement;
+            if (target.nodeName === 'LI') {
+                this.index = target.index;
+                this.showImg();
+                //this.autoplay();
+            }
+        }
+        this.ranNavBox.onmouseover = (e)=>{
+            e = e || event;
+            e.stopPropagation();
+            e.preventDefault();
+            var target = e.target || e.srcElement;
+            if(target.nodeName === 'A'){
+                this.count = target.count;
+                for(var i=0;i<this.ranNavAll.length;i++){
+                    this.ranNavAll[i].children[0].className = '';
+                    this.ranUlAll[i].style.display = 'none';
+                }
+                this.ranUlAll[this.count].style.display = 'block'; 
+                this.ranNavAll[this.count].children[0].className = 'a-red';      
+            }
+        }
+    }
+    showImg() {
+        if (this.index > this.rubIndexAll.length - 1) {
+            this.ranUlbox.style.left = 0;
+            this.index = 0;
+        }
+        for (var i = 0; i < this.rubIndexAll.length; i++) {
+            this.rubIndexAll[i].className = '';
+        }
+        this.rubIndexAll[this.index].className = 'eb3436';
+        animate(this.ranUlbox, {
+            param: {
+                left: -(this.index + 1) * 350
+            }
+        })
+    }
+    autoplay() {
+        clearInterval(this.timer);
+        this.timer = setInterval(() => {
+            this.index++;
+            this.showImg()
+        }, 3000);
+    }
+}
+
+class RanNav2{
+    constructor() {
+        this.ranUl = document.querySelectorAll('.ran-nav .ran-ul')[1];
+        this.ranUlAll = document.querySelectorAll('.ran-nav .ran-ul');
+        this.all = this.ranUl.querySelectorAll('.navBox a');
+        this.ranUlbox = this.ranUl.querySelector('.ran-ul-box');
+        this.ranNavBox = document.querySelector('.navBox');
+        this.ranNavAll = this.ranNavBox.children;
+        this.ranUlboxAll = this.ranUlbox.children;
+        this.flist = this.ranUlboxAll[0];
+        this.last = this.ranUlboxAll[this.ranUlboxAll.length - 1];
+        this.ranUlbox.appendChild(this.flist.cloneNode(true));
+        this.ranUlbox.insertBefore(this.last.cloneNode(true), this.flist);
+        this.rubIndex = this.ranUl.children[1];
+        this.rubIndexAll = this.rubIndex.children;
+        this.index = 0;
+        this.count = 0;
+        this.timer = null;
+        this.ranUlbox.style.left = -350 + 'px';
+        for (var i = 0; i < this.rubIndexAll.length; i++) {
+            this.rubIndexAll[i].index = i;
+        }
+        for(var j=0;j<this.ranNavAll.length;j++){
+            this.ranNavAll[j].count = j;
+            this.ranNavAll[j].children[0].count = j;
+        }
+      //  this.autoplay();
+    }
+    event() {
+        this.rubIndex.onclick = (e) => {
+            e = e || event;
+            var target = e.target || e.srcElement;
+            if (target.nodeName === 'LI') {
+                this.index = target.index;
+                this.showImg();
+               // this.autoplay();
+            }
+        }
+        this.ranNavBox.onmouseover = (e)=>{
+            e = e || event;
+            e.stopPropagation();
+            e.preventDefault();
+            var target = e.target || e.srcElement;
+            if(target.nodeName === 'A'){
+                this.count = target.count;
+                for(var i=0;i<this.ranNavAll.length;i++){
+                    this.ranNavAll[i].children[0].className = '';
+                    this.ranUlAll[i].style.display = 'none';
+                }
+                this.ranUlAll[this.count].style.display = 'block'; 
+                this.ranNavAll[this.count].children[0].className = 'a-red';      
+            }
+        }
+    }
+    showImg() {
+        if (this.index > this.rubIndexAll.length - 1) {
+            this.ranUlbox.style.left = 0;
+            this.index = 0;
+        }
+        for (var i = 0; i < this.rubIndexAll.length; i++) {
+            this.rubIndexAll[i].className = '';
+        }
+        this.rubIndexAll[this.index].className = 'eb3436';
+        animate(this.ranUlbox, {
+            param: {
+                left: -(this.index + 1) * 350
+            }
+        })
+    }
+    autoplay() {
+        clearInterval(this.timer);
+        this.timer = setInterval(() => {
+            this.index++;
+            this.showImg()
+        }, 3000);
+    }
+}
+
+class RanNav3{
+    constructor() {
+        this.ranUl = document.querySelectorAll('.ran-nav .ran-ul')[2];
+        this.ranUlAll = document.querySelectorAll('.ran-nav .ran-ul');
+        this.all = this.ranUl.querySelectorAll('.navBox a');
+        this.ranUlbox = this.ranUl.querySelector('.ran-ul-box');
+        this.ranNavBox = document.querySelector('.navBox');
+        this.ranNavAll = this.ranNavBox.children;
+        this.ranUlboxAll = this.ranUlbox.children;
+        this.flist = this.ranUlboxAll[0];
+        this.last = this.ranUlboxAll[this.ranUlboxAll.length - 1];
+        this.ranUlbox.appendChild(this.flist.cloneNode(true));
+        this.ranUlbox.insertBefore(this.last.cloneNode(true), this.flist);
+        this.rubIndex = this.ranUl.children[1];
+        this.rubIndexAll = this.rubIndex.children;
+        this.index = 0;
+        this.count = 0;
+        this.timer = null;
+        this.ranUlbox.style.left = -350 + 'px';
+        for (var i = 0; i < this.rubIndexAll.length; i++) {
+            this.rubIndexAll[i].index = i;
+        }
+        for(var j=0;j<this.ranNavAll.length;j++){
+            this.ranNavAll[j].count = j;
+            this.ranNavAll[j].children[0].count = j;
+        }
+       // this.autoplay();
+    }
+    event() {
+        this.rubIndex.onclick = (e) => {
+            e = e || event;
+            var target = e.target || e.srcElement;
+            if (target.nodeName === 'LI') {
+                this.index = target.index;
+                this.showImg();
+               // this.autoplay();
+            }
+        }
+        this.ranNavBox.onmouseover = (e)=>{
+            e = e || event;
+            e.stopPropagation();
+            e.preventDefault();
+            var target = e.target || e.srcElement;
+            if(target.nodeName === 'A'){
+                this.count = target.count;
+                for(var i=0;i<this.ranNavAll.length;i++){
+                    this.ranNavAll[i].children[0].className = '';
+                    this.ranUlAll[i].style.display = 'none';
+                }
+                this.ranUlAll[this.count].style.display = 'block'; 
+                this.ranNavAll[this.count].children[0].className = 'a-red';      
+            }
+        }
+    }
+    showImg() {
+        if (this.index > this.rubIndexAll.length - 1) {
+            this.ranUlbox.style.left = 0;
+            this.index = 0;
+        }
+        for (var i = 0; i < this.rubIndexAll.length; i++) {
+            this.rubIndexAll[i].className = '';
+        }
+        this.rubIndexAll[this.index].className = 'eb3436';
+        animate(this.ranUlbox, {
+            param: {
+                left: -(this.index + 1) * 350
+            }
+        })
+    }
+    autoplay() {
+        clearInterval(this.timer);
+        this.timer = setInterval(() => {
+            this.index++;
+            this.showImg()
+        }, 3000);
+    }
+}
+class RanNav4{
+    constructor() {
+        this.ranUl = document.querySelectorAll('.ran-nav .ran-ul')[3];
+        this.ranUlAll = document.querySelectorAll('.ran-nav .ran-ul');
+        this.all = this.ranUl.querySelectorAll('.navBox a');
+        this.ranUlbox = this.ranUl.querySelector('.ran-ul-box');
+        this.ranNavBox = document.querySelector('.navBox');
+        this.ranNavAll = this.ranNavBox.children;
+        this.ranUlboxAll = this.ranUlbox.children;
+        this.flist = this.ranUlboxAll[0];
+        this.last = this.ranUlboxAll[this.ranUlboxAll.length - 1];
+        this.ranUlbox.appendChild(this.flist.cloneNode(true));
+        this.ranUlbox.insertBefore(this.last.cloneNode(true), this.flist);
+        this.rubIndex = this.ranUl.children[1];
+        this.rubIndexAll = this.rubIndex.children;
+        this.index = 0;
+        this.count = 0;
+        this.timer = null;
+        this.ranUlbox.style.left = -350 + 'px';
+        for (var i = 0; i < this.rubIndexAll.length; i++) {
+            this.rubIndexAll[i].index = i;
+        }
+        for(var j=0;j<this.ranNavAll.length;j++){
+            this.ranNavAll[j].count = j;
+            this.ranNavAll[j].children[0].count = j;
+        }
+       // this.autoplay();
+    }
+    event() {
+        this.rubIndex.onclick = (e) => {
+            e = e || event;
+            var target = e.target || e.srcElement;
+            if (target.nodeName === 'LI') {
+                this.index = target.index;
+                this.showImg();
+                //this.autoplay();
+            }
+        }
+        this.ranNavBox.onmouseover = (e)=>{
+            e = e || event;
+            e.stopPropagation();
+            e.preventDefault();
+            var target = e.target || e.srcElement;
+            if(target.nodeName === 'A'){
+                this.count = target.count;
+                for(var i=0;i<this.ranNavAll.length;i++){
+                    this.ranNavAll[i].children[0].className = '';
+                    this.ranUlAll[i].style.display = 'none';
+                }
+                this.ranUlAll[this.count].style.display = 'block'; 
+                this.ranNavAll[this.count].children[0].className = 'a-red';      
+            }
+        }
+    }
+    showImg() {
+        if (this.index > this.rubIndexAll.length - 1) {
+            this.ranUlbox.style.left = 0;
+            this.index = 0;
+        }
+        for (var i = 0; i < this.rubIndexAll.length; i++) {
+            this.rubIndexAll[i].className = '';
+        }
+        this.rubIndexAll[this.index].className = 'eb3436';
+        animate(this.ranUlbox, {
+            param: {
+                left: -(this.index + 1) * 350
+            }
+        })
+    }
+    autoplay() {
+        clearInterval(this.timer);
+        this.timer = setInterval(() => {
+            this.index++;
+            this.showImg()
+        }, 3000);
+    }
+}
+class RanNav5{
+    constructor() {
+        this.ranUl = document.querySelectorAll('.ran-nav .ran-ul')[4];
+        this.ranUlAll = document.querySelectorAll('.ran-nav .ran-ul');
+        this.all = this.ranUl.querySelectorAll('.navBox a');
+        this.ranUlbox = this.ranUl.querySelector('.ran-ul-box');
+        this.ranNavBox = document.querySelector('.navBox');
+        this.ranNavAll = this.ranNavBox.children;
+        this.ranUlboxAll = this.ranUlbox.children;
+        this.flist = this.ranUlboxAll[0];
+        this.last = this.ranUlboxAll[this.ranUlboxAll.length - 1];
+        this.ranUlbox.appendChild(this.flist.cloneNode(true));
+        this.ranUlbox.insertBefore(this.last.cloneNode(true), this.flist);
+        this.rubIndex = this.ranUl.children[1];
+        this.rubIndexAll = this.rubIndex.children;
+        this.index = 0;
+        this.count = 0;
+        this.timer = null;
+        this.ranUlbox.style.left = -350 + 'px';
+        for (var i = 0; i < this.rubIndexAll.length; i++) {
+            this.rubIndexAll[i].index = i;
+        }
+        for(var j=0;j<this.ranNavAll.length;j++){
+            this.ranNavAll[j].count = j;
+            this.ranNavAll[j].children[0].count = j;
+        }
+        //this.autoplay();
+    }
+    event() {
+        this.rubIndex.onclick = (e) => {
+            e = e || event;
+            var target = e.target || e.srcElement;
+            if (target.nodeName === 'LI') {
+                this.index = target.index;
+                this.showImg();
+                console.log(1)
+             //   this.autoplay();
+            }
+        }
+        this.ranNavBox.onmouseover = (e)=>{
+            e = e || event;
+            e.stopPropagation();
+            e.preventDefault();
+            var target = e.target || e.srcElement;
+            if(target.nodeName === 'A'){
+                this.count = target.count;
+                for(var i=0;i<this.ranNavAll.length;i++){
+                    this.ranNavAll[i].children[0].className = '';
+                    this.ranUlAll[i].style.display = 'none';
+                }
+                this.ranUlAll[this.count].style.display = 'block'; 
+                this.ranNavAll[this.count].children[0].className = 'a-red';      
+            }
+        }
+    }
+    showImg() {
+        if (this.index > this.rubIndexAll.length - 1) {
+            this.ranUlbox.style.left = 0;
+            this.index = 0;
+        }
+        for (var i = 0; i < this.rubIndexAll.length; i++) {
+            this.rubIndexAll[i].className = '';
+        }
+        this.rubIndexAll[this.index].className = 'eb3436';
+        animate(this.ranUlbox, {
+            param: {
+                left: -(this.index + 1) * 350
+            }
+        })
+    }
+    autoplay() {
+        clearInterval(this.timer);
+        this.timer = setInterval(() => {
+            this.index++;
+            this.showImg()
+        }, 3000);
+    }
+}
 window.onload = function () {
     new Banner().imgshow('.banner');
     new Li2().event();
     new XiaoLubo().event();
     new CountDown().getHDTime();
+    new RanNav().event();
+    new RanNav2().event();
+    new RanNav3().event();
+    new RanNav4().event();
+    new RanNav5().event();
 }
