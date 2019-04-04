66783fb509f13204d528f437570d26c5da14a5d7
Andy1027703354 <1027703354@qq.com>
2019-04-04 16:44:37 +0800

    修改了下

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-const css = require('gulp-minify-css');
-
-gulp.task('minicss', function() {
-  // 将你的默认的任务代码放在这
-      gulp.src('app/css/*.css')
-    .pipe(css())
-    .pipe(gulp.dest('dist/css/'));
-});
